c6783c43942fa179a65a1c7bead7037999e5fd79
Bill Shupp <bill@shupp.org>
2018-05-31 13:04:09 -0700

    Fixed issue with variables that may contain / breaking sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 CONSUL_KEY=${CONSUL_KEY:-VEGADNS-CHANGES}
 
 # Prep consul-template config file config
-sed -i "s/@@KEY@@/${CONSUL_KEY}/" /etc/consul-template.in.tpl
+sed -i "s!@@KEY@@!${CONSUL_KEY}!" /etc/consul-template.in.tpl
 
 exec "$@"
